1e2e683722f65aa8bdde10042e8ae88e2284ac89
Yogesh Kumar <yogesh_k@lattech.in>
2024-08-02 16:25:36 +0530

    input

---
+++ lib/components/InputField/InputField.stories.d.ts
@@ -0,0 +1,5 @@
+import { Meta } from '@storybook/react';
+import { InputFieldProps } from './InputField';
+declare const _default: Meta;
+export default _default;
+export declare const Default: import("@storybook/csf").AnnotatedStoryFn<import("@storybook/react").ReactRenderer, InputFieldProps>;
